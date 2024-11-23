07dc635558321b27e3ba7693a31dfe8ea531f64d
rao-at-406963944182 <rao>
2024-11-23 22:14:44 +0500

    Dockerfile added

--- guatecompras/nog.py
+++ guatecompras/nog.py
@@ -5,6 +5,8 @@ import time
 import sqlite3
 import requests
 import time
+import random
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
